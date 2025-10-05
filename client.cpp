b6761ca7f70a6abf0aec4ad46d55827db80980b6
SkyNaoto <naoto_70102@yahoo.co.jp>
2025-10-05 12:16:53 +0900

    server, client に分けて、client から TCP socket 通信させるように更新

---
+++ client.cpp
@@ -0,0 +1,65 @@
+#include "menu.pb.h"
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// プロトタイプ宣言
+void SendProtobuf(int sock, const google::protobuf::Message& message);
+
+int main() {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    // データ作成
+    MenuTree menutree;
+    menutree.set_language("Japanese");
+    menutree.set_parent_id(1001);
+    menutree.set_status(true);
+
+    item* item1 = menutree.add_items();
+    item1->set_message_text("Gauge");
+    item1->set_message_id("Message_1000");
+    item1->set_chiled_id(1051);
+    item1->set_data_id("LCAN_FF40");
+
+    item* item2 = menutree.add_items();
+    item2->set_message_text("HVAC");
+    item2->set_message_id("Message_2000");
+    item2->set_chiled_id(2051);
+    item2->set_data_id("LCAN_FF50");;
+
+    // TCP送信
+    // TCPソケット作成 AF_INET IPv4, SOCK_STREAM TCP, 0 プロトコルは自動選択
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    // サーバーアドレス設定
+    sockaddr_in serv_addr{};
+    // IPv4
+    serv_addr.sin_family = AF_INET;
+    // ポート番号
+    serv_addr.sin_port = htons(8080);
+    // IPアドレス localhost
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    // サーバーに接続
+    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+     // メッセージ送信
+    SendProtobuf(sock, menutree);
+
+    close(sock);
+    google::protobuf::ShutdownProtobufLibrary();
+}
+
+// TCPでprotobufメッセージを送信する関数
+void SendProtobuf(int sock, const google::protobuf::Message& message) {
+    //シリアライズ　バイナリー化
+    std::string out;
+    message.SerializeToString(&out);
+
+    // htonl()は、ホストバイトオーダーからネットワークバイトオーダー（ビッグエンディアン）に変換する関数
+    uint32_t msg_size = htonl(out.size());
+    // メッセージサイズを先頭に送信し、続けてメッセージ本体を送信
+    send(sock, &msg_size, sizeof(msg_size), 0);
+    send(sock, out.data(), out.size(), 0);
+    std::cout << "📤 MenuTree message sent (" << out.size() << " bytes)" << std::endl;
+}
---
+++ server.cpp
@@ -0,0 +1,94 @@
+#include "menu.pb.h"
+#include <iostream>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <google/protobuf/message.h>
+
+bool ReceiveProtobuf(int client_socket, google::protobuf::Message& message) ;
+
+// ------------------------
+// メインサーバー処理
+// ------------------------
+int main() {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    // TCPサーバー設定
+    // TCPソケット作成 AF_INET IPv4, SOCK_STREAM TCP, 0 プロトコルは自動選択
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    // サーバーアドレス設定 アドレスとはIPアドレスとポート番号の組み合わせ
+    sockaddr_in address{};
+    // IPv4
+    address.sin_family = AF_INET;
+    // すべてのアドレスからの接続を受け入れる
+    address.sin_addr.s_addr = INADDR_ANY;
+    // ポート番号
+    address.sin_port = htons(8080);
+
+    // ソケットにアドレスをバインドし、接続待ち状態にする seizeof(address)はアドレス構造体のサイズ
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    // 接続待ち状態にする 最大1つの接続を待機
+    listen(server_fd, 1);
+
+    std::cout << "📡 Server listening on port 8080..." << std::endl;
+
+    // クライアント接続待ち addressは接続してきたクライアントのアドレス情報が入る
+    int addrlen = sizeof(address);
+    // クライアントからの接続を受け入れる
+    int client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+
+    MenuTree menutree;
+    // menutreeにデータを受信
+    if (ReceiveProtobuf(client_socket, menutree)) {
+        std::cout << "✅ Message received successfully!" << std::endl;
+        std::cout << "Language: " << menutree.language() << std::endl;
+        std::cout << "Parent ID: " << menutree.parent_id() << std::endl;
+        std::cout << "Status: " << (menutree.status() ? "true" : "false") << std::endl;
+
+        for (const auto& i : menutree.items()) {
+            std::cout << "  " << i.message_text()
+                      << " (" << i.message_id()
+                      << ", " << i.chiled_id()
+                      << ", " << i.data_id()
+                      << ")" << std::endl;
+        }
+    }
+
+    close(client_socket);
+    close(server_fd);
+    google::protobuf::ShutdownProtobufLibrary();
+}
+
+
+// 汎用protobuf受信関数
+// client_socket: クライアントソケット message: 受信したprotobufメッセージを格納するオブジェクト
+bool ReceiveProtobuf(int client_socket, google::protobuf::Message& message) {
+    // メッセージサイズ受信 まず4バイトのサイズ情報を受信 その後データ本体を受信　ここではまず4バイトのサイズ情報を受信
+    uint32_t msg_size;
+    // 4バイトのメッセージサイズを先頭から受信 readはソケットからデータを読み込む関数 msg_sizeに読み込んだデータを格納
+    int n = read(client_socket, &msg_size, sizeof(msg_size));
+    // 4バイト読み込めなかった場合はエラー
+    if (n != sizeof(msg_size)) {
+        std::cerr << "Failed to read message size." << std::endl;
+        return false;
+    }
+    // ネットワークバイトオーダーからホストバイトオーダーに変換
+    msg_size = ntohl(msg_size);
+
+    // メッセージ本体を受信
+    // msg_sizeバイト分のバッファを確保し、そこにデータを読み込む
+    std::string buffer(msg_size, '\0');
+    // readでmsg_sizeバイト分のデータを読み込み、bufferに格納
+    int bytes_read = read(client_socket, buffer.data(), msg_size);
+    // 期待したバイト数を読み込めなかった場合はエラー
+    if (bytes_read != msg_size) {
+        std::cerr << "Failed to read full message body." << std::endl;
+        return false;
+    }
+
+    if (!message.ParseFromString(buffer)) {
+        std::cerr << "Failed to parse protobuf message." << std::endl;
+        return false;
+    }
+
+    return true;
+}
